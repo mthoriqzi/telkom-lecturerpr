5516f9be327b60dfcc2e3d6e39e993d33a9bd9f1
Muhamad Thoriq Ziyadatul Iman <74279079+mthoriqzi@users.noreply.github.com>
2022-08-18 02:26:16 +0700

    scatter plot

--- Client/src/pages/Cluster.jsx
+++ Client/src/pages/Cluster.jsx
@@ -5,9 +5,73 @@ import Axios from 'axios';
 import { ScatterChart, Scatter, XAxis, 
     YAxis, CartesianGrid } from 'recharts';
 import HasilCluster from '../components/hasilcluster/HasilCluster';
+// import App from "./App";
+import ReactApexChart from 'react-apexcharts';
+import ReactDOM from 'react-dom'
+import { useSelector } from 'react-redux'
+
+
+
 
 function Cluster() {
- 
+    const themeReducer = useSelector(state => state.ThemeReducer.mode)
+//     class ApexChart extends React.Component {
+//         constructor(props) {
+//           super(props);
+
+//           this.state = {
+          
+//             series: [{
+//               name: "Cluster 0",
+//               data: [dataList0["PC1_2d"],dataList0["PC2_2d"]
+//               ]
+//             },{
+//               name: "Cluster 1",
+//               data: [
+//               [1.4, 3.4]]
+//             },{
+//               name: "Cluster 2",
+//               data: [
+//               [21.7, 3]]
+//             }],
+//             options: {
+//               chart: {
+//                 height: 350,
+//                 type: 'scatter',
+//                 zoom: {
+//                   enabled: true,
+//                   type: 'xy'
+//                 }
+//               },
+//               xaxis: {
+//                 tickAmount: 10,
+//                 labels: {
+//                   formatter: function(val) {
+//                     return parseFloat(val).toFixed(1)
+//                   }
+//                 }
+//               },
+//               yaxis: {
+//                 tickAmount: 7
+//               }
+//             },
+          
+          
+//           };
+//         }
+
+//         render() {
+//             return (
+              
+  
+//         <div id="chart">
+//     <ReactApexChart options={this.state.options} series={this.state.series} type="scatter" height={350} />
+//   </div>
+//             );
+//         }
+//       }
+    // const domContainer = document.querySelector('#app');
+
     const [clusterList, setClusterList] = useState([])
     const [dataList, setDataList] = useState([])
     const [dataList0, setDataList0] = useState([])
@@ -18,26 +82,33 @@ function Cluster() {
         Axios.get('http://localhost:3001/api/get-cluster').then((response) => {
             setDataList(response.data);
         });
-        //   dataList.setHeader("Access-Control-Allow-Origin", "*");
-    }, []);
-    useEffect(() => {
-        Axios.get('http://localhost:3001/api/get-cluster-0').then((response) => {
+        Axios.get('http://localhost:3001/api/get-cluster-0',{ mode: 'cors' }).then((response) => {
             setDataList0(response.data);
         });
-        //   dataList.setHeader("Access-Control-Allow-Origin", "*");
-    }, []);
-    useEffect(() => {
         Axios.get('http://localhost:3001/api/get-cluster-1').then((response) => {
             setDataList1(response.data);
         });
-        //   dataList.setHeader("Access-Control-Allow-Origin", "*");
-    }, []);
-    useEffect(() => {
         Axios.get('http://localhost:3001/api/get-cluster-2').then((response) => {
             setDataList2(response.data);
         });
+        
         //   dataList.setHeader("Access-Control-Allow-Origin", "*");
     }, []);
+
+    // useEffect(() => {
+    //     Axios.get('http://localhost:3001/api/get-cluster-0',{ mode: 'cors' }).then((response) => {
+    //         setDataList0(response.data);
+    //     });
+        //   dataList0.setHeader("Access-Control-Allow-Origin", "*");
+    // }, []);
+    // useEffect(() => {
+
+    //     //   dataList1.setHeader("Access-Control-Allow-Origin", "*");
+    // }, []);
+    // useEffect(() => {
+
+    //     //   dataList2.setHeader("Access-Control-Allow-Origin", "*");
+    // }, []);
 
     const customerTableHead = [
         'KODE NAMA',
@@ -65,6 +136,69 @@ function Cluster() {
             <td>{item.abdimas_diakui}</td>
         </tr>
     )
+    var value0 = []
+    var value1 = []
+    var value2 = []
+    // for (var i in dataList0.length){
+    //     value0[i]=[dataList0["PC1_2d"][i], dataList0["PC2_2d"][i]]
+    // }
+    console.log(typeof(dataList))
+    if (dataList0.length!=0){
+        // console.log(dataList0)
+        dataList0.map((data, index) =>{
+            value0[index]=[data["PC1_2d"], data["PC2_2d"].toFixed(2)]
+        })
+
+    }
+    // console.log(value0)
+    if (dataList1.length!=0){
+        for (var i in dataList1){
+            value1[i]=[dataList1[i]["PC1_2d"], dataList1[i]["PC2_2d"].toFixed(2)]
+        }
+
+    }
+    // console.log(value1)
+    if (dataList2.length!=0){
+        for (var i in dataList2){
+            value2[i]=[dataList2[i]["PC1_2d"], dataList2[i]["PC2_2d"].toFixed(2)]
+        }
+
+    }
+
+    const scatterCluster = {
+        series: [{
+            name: "Cluster 0",
+            data: value0
+          },{
+            name: "Cluster 1",
+            data: value1
+          },{
+            name: "Cluster 2",
+            data: value2
+          }],
+          options: {
+            chart: {
+              height: 350,
+              type: 'scatter',
+              zoom: {
+                enabled: true,
+                type: 'xy'
+              }
+            },
+            xaxis: {
+              tickAmount: 10,
+              labels: {
+                formatter: function(val) {
+                  return parseFloat(val).toFixed(1)
+                }
+              }
+            },
+            yaxis: {
+              tickAmount: 7
+            }
+          },
+    
+    }
 
     // useEffect(() => {
     //   Axios.get('http://127.0.0.1:5000').then((response) => {
@@ -75,9 +209,11 @@ function Cluster() {
     // console.log(clusterList)
     
     return (
-        
+        // ReactDOM.render(React.createElement(ApexChart), document.getElementById('root')),
         <div>
-            {dataList.length!=0 &&
+            
+            {/* <div id="scatter"></div> */}
+            {/* {dataList.length!=0 &&
             <div>
                 <ScatterChart width={400} height={400}>
                 <CartesianGrid />
@@ -86,10 +222,33 @@ function Cluster() {
                 <Scatter post={dataList} fill="green" />
                 </ScatterChart>
             </div>
-            }
+            } */}
+            <h2>Hasil Cluster Periode:</h2>
+            <br></br>
+            {dataList0.length!=0 & dataList1.length!=0 & dataList2.length!=0 && 
+            
+            <div className="col-6">
+                <div className="card full-height min-height-500">
+               
+                        {/* chart */}
+                        <ReactApexChart
+                            options={themeReducer === 'theme-mode-dark' ? {
+                                ...scatterCluster.options,
+                                theme: { mode: 'dark'}
+                            } : {
+                                ...scatterCluster.options,
+                                theme: { mode: 'light'}
+                            }}
+                            series={scatterCluster.series}
+                            type='scatter'
+                            height='100%'
+                        />
+                </div>
+            </div>
+}
             {dataList0.length!=0 & dataList1.length!=0 & dataList2.length!=0 && 
             <div>
-                <h2>Hasil Cluster Periode:</h2>
+                
                 <h1>Cluster 0</h1>
                 <div className="row">
                     <div className="col-12">
--- backend/test.py
+++ backend/test.py
@@ -18,6 +18,7 @@ import time, datetime
 from datetime import datetime
 import json
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -25,6 +26,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:password@localhost:3306/DB'
 engine = create_engine('mysql://root:password@localhost:3306/DB')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
 
 db = SQLAlchemy(app)
 session = db.session()
@@ -50,12 +52,17 @@ PCs_2d.columns = ["PC1_2d", "PC2_2d"]
 PCs_3d.columns = ["PC1_3d", "PC2_3d", "PC3_3d"]
 td_x = pd.concat([td_x,PCs_1d,PCs_2d,PCs_3d], axis=1, join='inner')
 
+
 cluster0 = td_x[td_x["cluster"] == 0]
 cluster1 = td_x[td_x["cluster"] == 1]
 cluster2 = td_x[td_x["cluster"] == 2]
+td_r = pd.concat([td_r,PCs_1d,PCs_2d,PCs_3d], axis=1, join='inner')
 # init_notebook_mode(connected=True)
 td_r.to_csv('./hasil clustering.csv', index= False)
-query_createtable = """ create table IF NOT EXISTS hasil_cluster (no int, kode_nama varchar(255), kode varchar(255), no_urut int, program_studi varchar(255), status_kepegawaian varchar(255), jfa varchar(255), dik_diakui double, lit_diakui double, abdimas_diakui double, penunjang double, prof_diakui double, total_sks double, pemenuhan_tridarma varchar(255), cluster int) """
+# td_x.to_csv('./hasil scatter.csv', index= False)
+
+
+query_createtable = """ create table IF NOT EXISTS hasil_cluster (no int, kode_nama varchar(255), kode varchar(255), no_urut int, program_studi varchar(255), status_kepegawaian varchar(255), jfa varchar(255), dik_diakui double, lit_diakui double, abdimas_diakui double, penunjang double, prof_diakui double, total_sks double, pemenuhan_tridarma varchar(255), cluster int, PC1_1d double, PC1_2d double, PC2_2d double, PC1_3d double, PC2_3d double, PC3_3d double) """
 session.execute(query_createtable)
 session.commit()
 
@@ -74,11 +81,17 @@ prof_diakui = td_r['prof_diakui']
 total_sks = td_r['total_sks']
 pemenuhan_tridarma = td_r['pemenuhan_tridarma']
 cluster = td_r['cluster']
+PC1_1d = td_r['PC1_1d']
+PC1_2d = td_r['PC1_2d']
+PC2_2d = td_r['PC2_2d']
+PC1_3d = td_r['PC1_3d']
+PC2_3d = td_r['PC2_3d']
+PC3_3d = td_r['PC3_3d']
 
 data_values = ""
 
 for r in range(0, len(td_r)):
-    values = "(" + str(no[r]) +",'"+ kode_nama[r] +"','"+ kode[r] +"',"+ str(no_urut[r]) +",'"+ program_studi[r] +"','"+ status_kepegawaian[r] +"','"+ jfa[r] +"',"+ str(dik_diakui[r]) +","+ str(lit_diakui[r]) +","+ str(abdimas_diakui[r]) +","+ str(penunjang[r]) +","+ str(prof_diakui[r]) +","+ str(total_sks[r]) +",'"+ pemenuhan_tridarma[r] +"',"+ str(cluster[r]) + "), "
+    values = "(" + str(no[r]) +",'"+ kode_nama[r] +"','"+ kode[r] +"',"+ str(no_urut[r]) +",'"+ program_studi[r] +"','"+ status_kepegawaian[r] +"','"+ jfa[r] +"',"+ str(dik_diakui[r]) +","+ str(lit_diakui[r]) +","+ str(abdimas_diakui[r]) +","+ str(penunjang[r]) +","+ str(prof_diakui[r]) +","+ str(total_sks[r]) +",'"+ pemenuhan_tridarma[r] +"',"+ str(cluster[r]) +","+ str(PC1_1d[r]) +","+ str(PC1_2d[r]) +","+ str(PC2_2d[r]) +","+ str(PC1_3d[r]) +","+ str(PC2_3d[r]) +","+ str(PC3_3d[r]) + "), "
     data_values += values
 
 session.execute("insert into hasil_cluster values " + data_values[:-2] + ";")
@@ -89,13 +102,20 @@ session.execute("insert into hasil_cluster values " + data_values[:-2] + ";")
 session.commit()
 session.close()
 
+CORS(app)
+# app.config['CORS_HEADERS'] = 'Content-Type'
+# cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
 #routes
 @app.route('/', methods=['POST', 'GET', 'DELETE'])
+@cross_origin()
 def users():
     # gets all users
     if request.method=='GET':
 
         df = pd.read_csv('hasil clustering.csv')
+        # .headers.add("Access-Control-Allow-Origin", "*")
+
         return {'hasil clustering': df.to_dict()}, 200
 
 # @app.route('/', methods=['POST'])
